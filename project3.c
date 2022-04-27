75a059ef25b4a246fddcb7ac4372a95f1064a72d
Grayson Hackett <44790226+GraysonHackett@users.noreply.github.com>
2022-04-27 17:14:31 -0400

    Update project3.c

--- project3.c
+++ project3.c
@@ -35,6 +35,7 @@ void c_shell(){
 	while(1){
 		// get the command from user
 		get_cmd();
+		// put the command into argv
 		convert_cmd();
 
 		// bypass empty commands
@@ -42,57 +43,55 @@ void c_shell(){
 			continue;
 		}
 
-		// check for "exit" commandf
+		// check for "exit" command
         if(!strcmp(argv[0],"exit")){
-            exit(0);
+            for(int j = 0; j < 5; j++){
+				exit(0);					// temporary loop to force an exit because sometimes exit doesnt work
+				j++
+			}
         }
 
 		// built in cd command
 		if (!strcmp(argv[0], "cd")){
+			// takes the dir from stdin for chdir()
 			directory = argv[1];
+
 			// prints error messafe if args were empty (instead of seg fault) or if dir not foudn
 			if(chdir(directory) == -1 || strlen(directory) == 0){
 				write(STDERR_FILENO, error_message, strlen(error_message));			
 				}
 			continue;
+			// if you don't continue, chdir() will get stuck parsing 
         }  
 
-		// fit the command into *argv[]
 
 		// fork and execute the command
 		pid = fork();
 		if(-1 == pid){
-			printf("failed to create a child\n");
+			write(STDERR_FILENO, error_message, strlen(error_message));			
 		}
 		else if(0 == pid){
-			// printf("hello from child\n");
-			// execute a command
+			// execute command
 			execvp(argv[0], argv);
 		}
 		else{
-			// printf("hello from parent\n");
 			// wait for the command to finish if "&" is not present
 			if(NULL == argv[i]) waitpid(pid, NULL, 0);
 		}
 	}
 }
 
-// change-directory built in command
-
-
 
 void get_cmd(){
-	// get command from user
 	printf("ccsh> ");
-	fgets(cmd, MAX_SIZE_CMD, stdin);
-	// remove trailing newline
-	if ((strlen(cmd) > 0) && (cmd[strlen (cmd) - 1] == '\n'))
+	fgets(cmd, MAX_SIZE_CMD, stdin);				// gets command from user
+
+	if ((strlen(cmd) > 0) && (cmd[strlen (cmd) - 1] == '\n')) 		// removes the trailing newline
         	cmd[strlen (cmd) - 1] = '\0';
 }
 
 void convert_cmd(){
-	// split string into argv
-	char *ptr;
+	char *ptr;				// split the string into argv
 	i = 0;
 	ptr = strtok(cmd, " ");
 	while(ptr != NULL){
