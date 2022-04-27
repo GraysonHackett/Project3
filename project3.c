dbdfdfc67ff5751f95c6adec7574082a089566c2
Grayson Hackett <44790226+GraysonHackett@users.noreply.github.com>
2022-04-27 17:19:35 -0400

    Update project3.c

--- project3.c
+++ project3.c
@@ -5,78 +5,64 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <signal.h>
-
 #define	MAX_SIZE_CMD	256
 #define	MAX_SIZE_ARG	16
-#define TAM 1000
 
 char cmd[MAX_SIZE_CMD];				// string holder for the command
 char *argv[MAX_SIZE_ARG];			// an array for command and arguments
-pid_t pid;					// global variable for the child process ID
-char i;						// global for loop counter
-char *directory;			// path for the directory 
+pid_t pid;							// global variable for the child process ID
+char i;								// global for loop counter
+char *directory;					// path for the directory 
 
-void get_cmd();					// get command string from the user
-void convert_cmd();				// convert the command string to the required format by execvp()
-void c_shell();					// to start the shell
+void get_cmd();						// get command string from the user
+void convert_cmd();					// convert the command string to the required format by execvp()
+void c_shell();						// to start the shell
 
 char error_message[30] = "An error has occurred\n"; // error message
 
-int main(){
-	// tie the handler to the SGNCHLD signal
-
-	// start the shell
-	c_shell();
+int main(){						
+	c_shell();						// start the shell
 
 	return 0;
 }
 
 void c_shell(){
 	while(1){
-		// get the command from user
-		get_cmd();
-		// put the command into argv
-		convert_cmd();
+		get_cmd();					// get the command from user
+		convert_cmd(); 				// put the command into argv
 
-		// bypass empty commands
-		if(!strcmp(argv[0],"")){
+		if(!strcmp(argv[0],"")){	// bypass empty commands
 			continue;
 		}
 
-		// check for "exit" command
-        if(!strcmp(argv[0],"exit")){
+        if(!strcmp(argv[0],"exit")){		// check for "exit" command (built-in)
             for(int j = 0; j < 5; j++){
 				exit(0);					// temporary loop to force an exit because sometimes exit doesnt work
-				j++
+				j++;
 			}
         }
 
-		// built in cd command
-		if (!strcmp(argv[0], "cd")){
-			// takes the dir from stdin for chdir()
-			directory = argv[1];
+		if (!strcmp(argv[0], "cd")){		// built in cd command
+			directory = argv[1];			// takes the dir from stdin for chdir()
 
-			// prints error messafe if args were empty (instead of seg fault) or if dir not foudn
-			if(chdir(directory) == -1 || strlen(directory) == 0){
+			if(chdir(directory) == -1 || strlen(directory) == 0){			// prints error messafe if args were empty (instead of seg fault) or if dir not found
 				write(STDERR_FILENO, error_message, strlen(error_message));			
 				}
-			continue;
-			// if you don't continue, chdir() will get stuck parsing 
+			continue;			// if you don't continue, chdir() will get stuck parsing 
+
         }  
 
 
-		// fork and execute the command
-		pid = fork();
+		pid = fork();		// fork and execute the command
 		if(-1 == pid){
 			write(STDERR_FILENO, error_message, strlen(error_message));			
 		}
 		else if(0 == pid){
-			// execute command
-			execvp(argv[0], argv);
+			execvp(argv[0], argv);		// execute command
+
 		}
 		else{
-			// wait for the command to finish if "&" is not present
-			if(NULL == argv[i]) waitpid(pid, NULL, 0);
+			if(NULL == argv[i]) waitpid(pid, NULL, 0);			// wait for the command to finish if "&" is not present
 		}
 	}
 }
@@ -100,8 +86,7 @@ void convert_cmd(){
 		ptr = strtok(NULL, " ");
 	}
 
-	// check for "&"
-	if(!strcmp("&", argv[i-1])){
+	if(!strcmp("&", argv[i-1])){	// check for "&" to allow for parallel commands
 	argv[i-1] = NULL;
 	argv[i] = "&";
 	}else{
