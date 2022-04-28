47f4971dba61caa0b8c4a17ad13e741e4509089a
Grayson Hackett <44790226+GraysonHackett@users.noreply.github.com>
2022-04-28 17:54:52 -0400

    Update path.c

--- path.c
+++ path.c
@@ -2,5 +2,5 @@ if (!strcmp(args[0],"path")){
 		char *new_path = malloc(sizeof(char)*1000);
 		new_path = args[1];
 		snprintf(cur_path, sizeof(char)*1000,"%s%s",cur_path,new_path);
-		setenv("$PATH",cur_path,0);
+		setenv("$PATH",cur_path,1);
 	}
